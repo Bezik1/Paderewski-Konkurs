d31b4145cf8c22f43e7868e92a15b1d61b79e14d
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2024-04-10 18:55:22 +0200

    Adding lighter textures

--- src/const/paths.ts
+++ src/const/paths.ts
@@ -1,2 +1,2 @@
 export const EARTH_TEXTURE = "earthpack/2k_earth_alpha_map.png";
-export const SPECULAR_TEXTURE = "earthpack/4k_earth_specular_orange_version_without_black.png";
+export const SPECULAR_TEXTURE = "earthpack/2k_earth_specular_orange_version_without_black.png";
