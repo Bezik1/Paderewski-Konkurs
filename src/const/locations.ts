8799ceeba2f82ada3354941ad0f5d5f49ae63ec5
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2024-04-10 19:02:05 +0200

    Adding Holandia to Map

--- src/const/locations.ts
+++ src/const/locations.ts
@@ -4,7 +4,7 @@ import { Location } from "../types/Location";
 export const STRASBURG: Location = {
     name: "Strasburg",
     text: 'Paderewski początkowo żyjąc w biedzie poznał wiążące się z nią troski. Pracując jako pedagog w konserwatorium w Strasburgu przez wiele tygodni odkładał pieniądze, by pozwolić sobie na wyjazd do Paryża i wysłuchanie koncertu Rubinsteina, który to wcześniej podczas serdecznego spotkania zaprosił go do przybycia na jeden ze swoich występów w stolicy Francji. Po przybyciu na miejsce, podczas rozmowy z Rubinsteinem, Paderewski dowiedział się, że pianista nie zajmuje się sprzedażą biletów. Wszystkie wejściówki zostały już dawno wyprzedane, w sali Erarda mogło zmieścić się jedynie około 700 osób. Mimo usilnych starań i licznych wyrzeczeń młodemu muzykowi nie udało się usłyszeć gry Rubinsteina. Nie usłyszał jej już nigdy.„Człowiekowi żyjącemu w dobrobycie i zbytku jest niezmiernie trudno zrozumieć, że ktoś inny może cierpieć biedę. Wymaga to dużego zasobu wyboraźni, i lepiej by się działo, gdyby ludzie częściej chceili się na nią zdobyć.”Paderewski nigdy nie odmówił nikomu, kto pragnął słuchać jego gry. Nawet gdy bilety nie był już dostępne, zawsze starał się zapewnić chętnemu słuchaczowi jakieś miesjce, ostatecznie nawet dostawiając stołek lub krzesło za estradą.',
-    position: new Vector3(0.1, 0.35, 2),
+    position: new Vector3(0.15, 0.35, 2),
     rotateTo: 0
 }
 
@@ -29,4 +29,11 @@ const RUSSIA: Location = {
     rotateTo: Math.PI * (-0.2)
 }
 
-export const LOCATIONS: Location[] = [AUSTRALIA, AMERICA, RUSSIA, STRASBURG]
+const HOLANDIA: Location = {
+    name: "Holandia",
+    text: ' Paderewski odbył osobliwe tournee po Holadnii, na które składały się koncerty m.in. w Hadze i Arnhem. Wszystko zaczęło się w 1889r. w Paryżu, gdzie pianistę odwiedził pewien dżentelmen, który zaproponował mu trasę koncertową na korzystnych warunkach. Paderewski miał być wspierany jeszcze przez dwóch innych artystów. Wbrew zapewnieniom impresaria frekwencja na koncertach była daleka od satyfakcjonującej. Paderewski nie zarobił ani grosza, a ponadto, po każdym koncercie pokrywał rachunki za znakomite kolacje dla wszystkich sześciu uczestników trasy koncertowej. Po tym, gdy uczynił ten szlachetny gest po pierwszym koncercie, jego towarzysze podróży uznali za naturalne, że będzie robił to po każdym kolejnym wsytępie. Przeznaczył na ten cel kilka tysięcy franków.',
+    position: new Vector3(0.1, 0.49, 2),
+    rotateTo: 0
+}
+
+export const LOCATIONS: Location[] = [AUSTRALIA, AMERICA, RUSSIA, HOLANDIA, STRASBURG]
